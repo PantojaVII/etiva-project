aacdfd18c15f9a2356caa6df446aa65d8cf56551
Pantoja <jvpantojamaciel@gmail.com>
2024-11-05 02:25:10 -0300

    Fix: Libera o cadastro de novos usuários

--- djangoapp/authentication/admin.py
+++ djangoapp/authentication/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User, Profile
 
-#@admin.register(User)
+@admin.register(User)
 class UserAdmin(BaseUserAdmin):
     list_display = ('email', 'username', 'auth_method', 'is_active', 'is_staff', 'is_superuser', 'id')
     list_filter = ('is_active', 'is_staff', 'is_superuser', 'auth_method')
